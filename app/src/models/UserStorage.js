3589e3e536b62bceec57e520442e531b9c3cc46e
rkddlwp <kyjkyj1024@naver.com>
2021-07-08 17:05:50 +0900

    User 로직 구현

--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -17,6 +17,18 @@ class UserStorage {  //static은 인스턴스를 생성 안해도 클래스에 �
     }, {});
     return newUsers;
   }
+
+  static getUserInfo(id) {
+    const users = this.#users;
+    const idx = users.id.indexOf(id);
+    const usersKeys = Object.keys(users); // => [id, psword, name]
+    const userInfo = usersKeys.reduce((newUser, info) => {
+      newUser[info] = users[info][idx];
+      return newUser;
+    }, {});
+
+    return userInfo;
+  }
 }
 
 module.exports = UserStorage;
